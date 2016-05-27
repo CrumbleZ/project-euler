19d89e144da8bede5f54ca8aece06bff48e48db1
Gabriel Freitas <frg.gabriel.freitas@gmail.com>
2016-05-27 17:42:13 +0200

    Solved problem 13 ; closes #13

---
+++ 0013.py
@@ -0,0 +1,8 @@
+"""
+
+Problem :
+    Work out the first ten digits of the sum of the following one-hundred 50-digit numbers.
+
+"""
+
+print(str(sum(int(number) for number in open("./data/0013.txt").read().splitlines()))[:10])
