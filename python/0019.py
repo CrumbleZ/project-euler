26389ceae27a34334cd49e25a4574b354580109e
Gabriel Freitas <frg.gabriel.freitas@gmail.com>
2016-05-30 22:39:46 +0200

    Solved problem 19 ; closes #19

---
+++ python/0019.py
@@ -0,0 +1,21 @@
+"""
+
+Problem :
+    How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
+
+"""
+
+from datetime import date
+from datetime import timedelta
+
+calendar = date(1901, 1, 1)
+limit = date(2000, 12, 31)
+
+answer = 0
+    
+while calendar <= limit:
+    calendar += timedelta(days=1)
+    if calendar.day == 1 and calendar.weekday() == 6:
+        answer += 1
+
+print(answer)
