c14e4b6be628aef26fc1363d84e458b5d9fb16a1
Gabriel Freitas <frg.gabriel.freitas@gmail.com>
2016-05-30 22:54:11 +0200

    Solved problem 20 ; close #20

---
+++ python/0020.py
@@ -0,0 +1,10 @@
+"""
+
+Problem :
+    How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
+
+"""
+
+from math import factorial
+
+print(sum(int(digit) for digit in str(factorial(100))))
