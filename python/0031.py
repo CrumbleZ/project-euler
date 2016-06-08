aee89c341c9dcf10a35e6463146d8731ec34c6bb
Gabriel Freitas <frg.gabriel.freitas@gmail.com>
2016-06-08 17:27:51 +0200

    Solved problem 31 ; closes #31

---
+++ python/0031.py
@@ -0,0 +1,19 @@
+"""
+
+Problem :
+    How many different ways can 2 pounds be made using any number of coins?
+
+"""
+
+coins = [1, 2, 5, 10, 20, 50, 100, 200]
+
+
+def count(change, coin_index):
+    if change < 0 or coin_index < 0:
+        return 0
+    if change == 0:
+        return 1
+
+    return count(change, coin_index - 1) + count(change - coins[coin_index], coin_index)
+
+print(count(200, len(coins) - 1))
