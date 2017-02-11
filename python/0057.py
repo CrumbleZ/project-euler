11b427e0c0213e0578d2d595583df45c7162f3ff
Gabriel Freitas <frg.gabriel.freitas@gmail.com>
2017-02-11 09:28:26 +0100

    closes #57

---
+++ python/0057.py
@@ -0,0 +1,39 @@
+"""
+
+Problem:
+    It is possible to show that the square root of two can be expressed as an infinite continued fraction.
+
+    √ 2 = 1 + 1/(2 + 1/(2 + 1/(2 + ... ))) = 1.414213...
+
+    In the first one-thousand expansions, how many fractions contain a numerator with more digits than denominator?
+
+
+"""
+
+import sys
+from fractions import Fraction
+
+sys.setrecursionlimit(1100)
+
+
+def estimate_sqrt2(cycles):
+    return 1 + Fraction(1/estimate_sqrt2_intern(cycles))
+
+
+def estimate_sqrt2_intern(cycles):
+    if cycles == 1:
+        return 2
+    elif cycles == 2:
+        return 2 + Fraction(1, 2)
+    elif cycles > 2:
+        return 2 + Fraction(1, estimate_sqrt2_intern(cycles - 1))
+
+result = 0
+
+for i in range(1, 1001):
+    if len(str(estimate_sqrt2(i).numerator)) > len(str(estimate_sqrt2(i).denominator)):
+        result += 1
+
+print(result)
+
+
