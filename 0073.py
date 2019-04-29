ede1a732c2aef85286c3e09cc04e1d26d205d1f0
CrumbleZ <frg.gabriel.freitas@gmail.com>
2019-04-29 13:23:01 +0200

    closes #73

---
+++ 0073.py
@@ -0,0 +1,31 @@
+"""
+
+Problem :
+    How many fractions lie between 1/3 and 1/2 in the sorted set of reduced
+    proper fractions for d ≤ 12,000?
+
+Performance time : ~3.8s
+
+"""
+
+from timer import timer
+from primes import list_primes
+from primes import prime_factors
+
+
+timer.start()
+
+#mark all possible denominators with their prime factors
+compositeness = {n: set(prime_factors(n)) for n in range(12001)}
+answer = 0
+
+for d in range(5, 12001):
+    d_factors = compositeness[d]
+    for n in range(d // 3, d // 2):
+        n_factors = compositeness[n]
+        if not n_factors & d_factors:
+            answer += 1
+
+print(answer)
+
+timer.stop()
