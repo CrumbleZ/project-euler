af9e45233f1c5ea621c309c65a0819ed55c562fa
CrumbleZ <frg.gabriel.freitas@gmail.com>
2019-05-06 12:05:51 +0200

    Improved problem 2

--- 0002.py
+++ 0002.py
@@ -4,29 +4,18 @@ Problem :
     By considering the terms in the Fibonacci sequence whose values
     do not exceed four million, find the sum of the even-valued terms.
 
-Nota Bene :
-    The problem states that sequence starts with 1 and 2 instead of 0 and 1
-
-Optimizations:
-    Needing only the even-valued number, we see that we get the repeating
-    sequence 'odd-even-odd' in the numbers generated. We can use that to
-    fasten the generation process.
-
 Performance time: ~0.00002s
 """
 
 from timer import timer
+from itertools import takewhile
 
-def fibonacci_custom(limit):
-    a, b = 1, 2
-    while b < limit:
-        yield b
-        a += b
-        b += a
-        a += b
-        a, b = b, a
-
+def fibonacci():
+    a, b = 2, 8
+    while True:
+        yield a
+        a, b = b, 4*b + a
 
 timer.start()
-print(sum(fibonacci_custom(4000000)))
+print(sum(n for n in takewhile(lambda x: x < 4000000, fibonacci())))
 timer.stop()
